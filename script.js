7e6153d6214546c94bf04ef602c59fe352522cc8
Kevin <kevin.nguyen2321@yahoo.com>
2023-08-25 16:18:28 -0500

    Add functionality buttons still no functionality for operations

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 let firstNum;
 let secondNum;
 let operator;
+let displayValue = "";
 
 
 const display = document.querySelector('.text');
 const buttons = document.querySelectorAll('.calc-btns')
 
+
 buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        let buttonText = button.textContent;
-        if (buttonText === 'AC') {
-            display.textContent = 0;
-            
-        } else if (buttonText === 'Clear') {
-            display.textContent = "";
-            
-            
-        } else  {
-            firstNum += display.textContent = buttonText;
-            
-        }
-            
-    });
+    button.addEventListener('click', handleBtnClick)
 });
 
 
@@ -54,7 +42,38 @@ function operate(operator,numOne,numTwo) {
             break;
         
         case "-":
-            subtract(numOne,numTwo)
+            subtract(numOne,numTwo);
             break;
     }
 }
+
+function handleBtnClick (e) {
+    const buttonText = e.target.textContent;
+    if (buttonText === 'AC') {
+        display.textContent = 0;
+        displayValue = "";
+        
+    } else if (buttonText === 'Clear') {
+        display.textContent = "";
+        displayValue = ""
+        
+    } else {
+        displayValue += buttonText;
+        display.textContent = displayValue;
+        if (displayValue.includes('+')) {
+            let splitValue = displayValue.split('')
+            console.log(splitValue)
+            let indexOp = splitValue.indexOf('+');
+            console.log(indexOp);
+            let numsBeforeOp = splitValue.slice(0,indexOp);
+            console.log(numsBeforeOp.join(''));
+            let numsAfterOp = splitValue.slice (indexOp + 1);
+            console.log(numsAfterOp.join(''));
+        }
+            
+
+
+    }
+}
+
+
