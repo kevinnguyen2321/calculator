f11cc738398e1726538cac2911895fb69f1bb2b7
Kevin <kevin.nguyen2321@yahoo.com>
2023-08-26 14:20:23 -0500

    Add functionality for one operator not for multiple as of yet

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-let firstNum;
-let secondNum;
-let operator;
+//DOM Variables and Display Variable//
 let displayValue = "";
-
-
 const display = document.querySelector('.text');
 const buttons = document.querySelectorAll('.calc-btns')
 
 
+//Event Listener for buttons//
 buttons.forEach(button => {
     button.addEventListener('click', handleBtnClick)
 });
 
 
-
-
-
-
-
-
+//Functions for Operations//
 function add (a,b) {
     return a + b;
 }
@@ -36,17 +28,24 @@ function divide(a,b) {
 }
 
 function operate(operator,numOne,numTwo) {
-    switch (operator) {
-        case "+" :
-            add(numOne,numOne);
-            break;
+    if (operator === '+') {
+        return add(parseFloat(numOne), parseFloat(numTwo));
+    
+    } else if (operator === '-') {
+        return subtract(parseFloat(numOne), parseFloat(numTwo));
         
-        case "-":
-            subtract(numOne,numTwo);
-            break;
+    } else if (operator === 'x') {
+        return multiply(parseFloat(numOne), parseFloat(numTwo));
+        
+    } else if (operator === '÷') {
+        return divide(parseFloat(numOne), parseFloat(numTwo));
+        
     }
 }
 
+
+
+// Function for each button event//
 function handleBtnClick (e) {
     const buttonText = e.target.textContent;
     if (buttonText === 'AC') {
@@ -60,20 +59,61 @@ function handleBtnClick (e) {
     } else {
         displayValue += buttonText;
         display.textContent = displayValue;
-        if (displayValue.includes('+')) {
-            let splitValue = displayValue.split('')
-            console.log(splitValue)
-            let indexOp = splitValue.indexOf('+');
-            console.log(indexOp);
-            let numsBeforeOp = splitValue.slice(0,indexOp);
-            console.log(numsBeforeOp.join(''));
-            let numsAfterOp = splitValue.slice (indexOp + 1);
-            console.log(numsAfterOp.join(''));
+        if (displayValue.includes('+') || displayValue.includes('-') || displayValue.includes ('÷') || displayValue.includes('x')) {
+            handleOperators(e);
         }
-            
-
-
     }
 }
 
 
+
+function handleOperators(e) {
+    let splitValue = displayValue.split('')
+    if (splitValue.includes('+')) {
+        let indexOp = splitValue.indexOf('+');
+        let firstNum = splitValue.slice(0,indexOp).join('');
+        let secondNum = splitValue.slice (indexOp + 1, splitValue.indexOf('=')).join('');
+        let operator = splitValue[indexOp];
+        if (displayValue.includes('=')) {
+            let result = operate(operator,firstNum, secondNum)
+            console.log(result)
+            display.textContent = result;
+        }
+    
+    } else if (splitValue.includes('-')) {
+        let indexOp = splitValue.indexOf('-');
+        let firstNum = splitValue.slice(0,indexOp).join('');
+        let secondNum = splitValue.slice (indexOp + 1, splitValue.indexOf('=')).join('');
+       let operator = splitValue[indexOp];
+       if (displayValue.includes('=')) {
+        let result = operate(operator,firstNum, secondNum)
+        console.log(result)
+        display.textContent = result;
+       }
+    
+    } else if (splitValue.includes('÷')) {
+        let indexOp = splitValue.indexOf('÷');
+        let firstNum = splitValue.slice(0,indexOp).join('');
+        let secondNum = splitValue.slice (indexOp + 1, splitValue.indexOf('=')).join('');
+       let operator = splitValue[indexOp];
+       if (displayValue.includes('=')) {
+        let result = operate(operator,firstNum, secondNum)
+        console.log(result)
+        display.textContent = result;
+       }
+
+        
+    } else if (splitValue.includes('x')) {
+        let indexOp = splitValue.indexOf('x');
+        let firstNum = splitValue.slice(0,indexOp).join('');
+        let secondNum = splitValue.slice (indexOp + 1, splitValue.indexOf('=')).join('');
+       let operator = splitValue[indexOp];
+       if (displayValue.includes('=')) {
+        let result = operate(operator,firstNum, secondNum)
+        console.log(result)
+        display.textContent = result;
+       }
+        
+    } 
+    
+}
